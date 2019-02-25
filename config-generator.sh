85d3927714fdd0d00687f754974e51526d40998f
Alexander Stefanov <astefanov@virtuozzo.com>
2019-02-25 23:46:43 +0300

    drop %disttag

--- config-generator.sh
+++ config-generator.sh
@@ -25,58 +25,22 @@ gen_included_repos() {
     printf '%s\n' '"""' >> "${default_cfg}"
 }
 
-if [ "${platform_arch}" = 'aarch64' ]; then
-cat <<EOF> $default_cfg
-config_opts['target_arch'] = '$platform_arch'
-config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'aarch64')
-EOF
-
-elif echo "${platform_arch}" |grep -qE '^armv[78]'; then
-cat <<EOF> $default_cfg
-config_opts['target_arch'] = '$platform_arch'
-config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'armv8hcnl', 'armv8hnl', 'armv8hl', 'armv7hnl', 'armv7hl', 'armv7l', 'aarch64')
-EOF
-
-elif [ "${platform_arch}" = 'riscv64' ]; then
-cat <<EOF> $default_cfg
-config_opts['target_arch'] = '$platform_arch'
-config_opts['legal_host_arches'] = ('riscv64')
-EOF
-
-elif [ "${platform_arch}" = 'znver1' ]; then
-cat <<EOF> $default_cfg
-config_opts['target_arch'] = '$platform_arch'
-config_opts['legal_host_arches'] = ('znver1', 'x86_64')
-EOF
-
-else
-
 cat <<EOF> $default_cfg
 config_opts['target_arch'] = '$platform_arch'
 config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64')
-EOF
-fi
-
-cat <<EOF>> $default_cfg
 config_opts['root'] = '$platform_name-$platform_arch'
 config_opts['chroot_setup_cmd'] = 'install @buildsys-build'
 config_opts['package_manager'] = 'yum'
 config_opts['useradd'] = '/usr/sbin/useradd -o -m -u %(uid)s -g %(gid)s -d %(home)s %(user)s'
 config_opts['releasever'] = '7' # at some point, this should be set by ABF
-config_opts['use_nspawn'] = False
-config_opts['tar'] = "tar"
 config_opts['basedir'] = '/var/lib/mock/'
 config_opts['cache_topdir'] = '/var/cache/mock/'
 
 config_opts['dist'] = 'res7'  # only useful for --resultdir variable subst
 config_opts['macros']['%packager'] = '$uname <$email>'
 config_opts['macros']['%_topdir'] = '%s/build' % config_opts['chroothome']
-config_opts['macros']['%_rpmfilename'] = '%%{NAME}-%%{VERSION}-%%{RELEASE}-%%{DISTTAG}.%%{ARCH}.rpm'
 config_opts['macros']['%cross_compiling'] = '0' # ABF should generally be considered native builds
 config_opts['plugin_conf']['ccache_enable'] = False
-#config_opts['plugin_conf']['root_cache_enable'] = '$rebuild_cache'
-#config_opts['plugin_conf']['root_cache_opts']['compress_program'] = "xz"
-#config_opts['plugin_conf']['root_cache_opts']['extension'] = ".xz"
 config_opts['yum.conf'] = """
 [main]
 keepcache=1
